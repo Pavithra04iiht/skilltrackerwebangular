6299278be13495fc7f843c834eeaecbaf27435aa
Pavithra04iiht <s.aradhyasri@gmail.com>
2022-12-11 19:02:46 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, ViewChild, HostListener, OnInit,NgZone } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { NavigationEnd ,Router} from '@angular/router';
+import { AuthenticationService } from './shared/authentication.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -9,15 +13,19 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class AppComponent {
   opened = true;
   isLoginPage=false;
-  @ViewChild('sidenav') sidenav: MatSidenav;
+  isUserLoggedIn=false;
+  userName="";
+  displayProgressbar=false;
+
+  @ViewChild('sidenav',{static:true}) sidenav: MatSidenav;
 
   ngOnInit() {
     console.log(window.innerWidth);
     if (window.innerWidth < 768) {
-      this.sidenav.fixedTopGap = 55;
+      if(this.sidenav) this.sidenav.fixedTopGap = 55;
       this.opened = false;
     } else {
-      this.sidenav.fixedTopGap = 55;
+      if(this.sidenav) this.sidenav.fixedTopGap = 55;
       this.opened = true;
     }
   }
@@ -25,10 +33,10 @@ export class AppComponent {
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     if (event.target.innerWidth < 768) {
-      this.sidenav.fixedTopGap = 55;
+      if(this.sidenav)  this.sidenav.fixedTopGap = 55;
       this.opened = false;
     } else {
-      this.sidenav.fixedTopGap = 55;
+      if(this.sidenav)  this.sidenav.fixedTopGap = 55;
       this.opened = true;
     }
   }
@@ -44,4 +52,44 @@ export class AppComponent {
       return false;
     }
   }
+
+  constructor(
+    private router:Router,
+    private ngZone:NgZone,
+    private authenticationservice:AuthenticationService){
+      this.initPage();
+      this.handleMenu();
+    
+    }
+
+    initPage(){
+      let user =this.authenticationservice.getLoggedInUser();
+      this.isUserLoggedIn=user.isLoggedIn;
+      this.userName=this.authenticationservice.getDisplayNmae();
+
+    }
+
+    public toggleProgressBar=(state:boolean)=>{
+      this.displayProgressbar=state;
+
+    };
+
+
+
+    public setUsername=(un:string)=>{ this.userName=un;}
+
+    handleMenu(){
+      this.router.events.subscribe((event:any)=>{
+        if(event instanceof NavigationEnd){
+          var pages=["/login","/register"]
+          this.isLoginPage=pages.includes(event.url);
+        }
+      });
+    }
+
+    logOut(){
+      this.authenticationservice.logout();
+      this.ngZone.run(()=>this.router.navigateByUrl("/login"));
+
+    }
 }
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,32 +1,52 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component ,NgZone,OnInit} from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { AuthenticationService } from '../shared/authentication.service';
+import { AppComponent } from '../app.component';
 
-import { IUser, CognitoService } from '../cognito.service';
+import { CognitoUserPool ,CognitoUser,AuthenticationDetails} from "amazon-cognito-identity-js";
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
-  loading: boolean;
-  user: IUser;
+  isLoading: boolean=false;
+  HasError=false;
+  ErrorMessage=false;
+  hide=true;
 
-  constructor(private router: Router,
-              private cognitoService: CognitoService) {
-    this.loading = false;
-    this.user = {} as IUser;
-  }
+  loginForm: FormGroup;
 
-  public signIn(): void {
-    this.loading = true;
-    this.cognitoService.signIn(this.user)
-    .then(() => {
-      this.router.navigate(['/profile']);
-    }).catch(() => {
-      this.loading = false;
-    });
-  }
+  constructor(
+    public fb:FormBuilder,
+    private router:Router,
+    private ngZone:NgZone,
+    private authenticationservice:AuthenticationService,
+    private appComponent:AppComponent
+  ){}
 
+ ngOnInit(): void {
+   this.initializeForm();
+ }
+
+ initializeForm(){
+  this.loginForm=this.fb.group({
+    username:["CTS",[Validators.required,Validators.minLength(5),Validators.maxLength(30)],],
+    password:["",[Validators.required,Validators.minLength(5),Validators.maxLength(30)],],
+  });
+
+  window.sessionStorage.clear();
+ }
+
+ public handleError=(controlName:string,errorName:string)=>{ return this.loginForm.controls[controlName].hasError(errorName);
+};
+
+onSignIn(){
+  this.appComponent.toggleProgressBar(true);
+
+}
 }
